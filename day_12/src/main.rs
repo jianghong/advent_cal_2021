731c3e815de6be87211cc1603c08b163a2109dad
Jackson Hong <jackson.hong@shopify.com>
2021-12-12 07:37:58 -0500

    day 12 p1

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,14 +1,22 @@
 use std::io::{BufRead, BufReader};
 use std::fs::File;
+use std::collections::HashMap;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Hash)]
 struct Cave {
     name: String,
     is_big: bool,
-    connected: Vec<Cave>
 }
 
-#[derive(Debug)]
+impl PartialEq for Cave {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
+
+impl Eq for Cave {}
+
+#[derive(Clone, Debug, PartialEq)]
 struct Connection {
     cave_a: Cave,
     cave_b: Cave
@@ -19,23 +27,31 @@ fn main() {
 }
 
 fn part1() {
-    let reader = BufReader::new(File::open("src/test.txt").unwrap());
+    let reader = BufReader::new(File::open("src/input.txt").unwrap());
     let mut caves: Vec<Cave> = Vec::new();
-    let mut connections: Vec<Connection> = Vec::new();
+    let mut connections_map: HashMap<Cave, Vec<Cave>> = HashMap::new();
+    let start_cave = Cave { name: "start".to_string(), is_big: false };
 
     for (i, line) in reader.lines().enumerate() {
         let line = line.unwrap();
-        let connection = parse_connection_from_line(&line);
-        caves.push(connection.cave_a.clone());
-        caves.push(connection.cave_b.clone());
-        connections.push(connection);
+        let connection_tuples = parse_connection_from_line(&line);
+        for connection_tuple in connection_tuples {
+            let connections = connections_map.get_mut(&connection_tuple.0);
+            if let Some(connection) = connections {
+                connection.push(connection_tuple.1);
+            } else {
+                connections_map.insert(connection_tuple.0.clone(), vec![connection_tuple.1.clone()]);
+            }
+        }
     }
 
-    println!("caves {:?}", caves);
-    println!("connections {:?}", connections);
+    let mut visited_caves: Vec<Cave> = vec![start_cave.clone()];
+    let total = num_paths_to_end(start_cave, &connections_map, visited_caves.clone());
+    println!("part 1 {}", total);
+    // println!("connections {:?}", connections_map.clone());
 }
 
-fn parse_connection_from_line(line: &str) -> Connection {
+fn parse_connection_from_line(line: &str) -> Vec<(Cave, Cave)> {
     let split = line.split("-").collect::<Vec<&str>>();
     let a = split[0];
     let b = split[1];
@@ -43,18 +59,37 @@ fn parse_connection_from_line(line: &str) -> Connection {
     let mut cave_a = Cave {
         name: a.to_string(),
         is_big: a.chars().next().unwrap().is_uppercase(),
-        connected: Vec::new()
     };
     let mut cave_b = Cave {
         name: b.to_string(),
         is_big: b.chars().next().unwrap().is_uppercase(),
-        connected: Vec::new()
     };
-    cave_a.connected.push(cave_b.clone());
-    cave_b.connected.push(cave_a.clone());
     
-    Connection {
-        cave_a: cave_a,
-        cave_b: cave_b
+    vec![(
+        cave_a.clone(),
+        cave_b.clone()
+    ),
+    (
+        cave_b.clone(),
+        cave_a.clone(),
+    )]
+}
+
+fn num_paths_to_end(cave: Cave, connections_map: &HashMap<Cave, Vec<Cave>>, visited_caves: Vec<Cave>) -> u64 {
+    let mut visited_caves = visited_caves.clone();
+    if cave.name == "end" {
+        return 1;
     }
+    if !cave.is_big {
+        visited_caves.push(cave.clone());
+    }
+
+    let mut num_paths = 0;
+    for connected_cave in connections_map.get(&cave).unwrap() {
+        if !visited_caves.contains(connected_cave) {
+            num_paths += num_paths_to_end(connected_cave.clone(), connections_map, visited_caves.clone());
+        }
+    }
+
+    num_paths
 }
